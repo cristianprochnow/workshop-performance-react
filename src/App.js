be0f0b62b76ccd5f8b412a6df75739479508046b
Cristian Prochnow <cristianprochnow@gmail.com>
2020-12-14 14:48:59 -0300

    feat: add code to implement windowing feature into listing

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react"
 import arrayMove from 'array-move';
 import { Feed } from "./Feed"
 
-
 function App() {
   const [posts, setPosts] = useState([])
   useEffect(() => {
--- src/Feed.js
+++ src/Feed.js
@@ -1,11 +1,34 @@
 import React from "react"
 import { Post } from "./Post"
 import { SortableContainer } from 'react-sortable-hoc';
+import { FixedSizeList as List } from 'react-window'
 
 export const Feed = SortableContainer(({ posts }) => {
-    return <ul>
-        {posts.map((post, index) => (
-            <Post key={index} index={index} post={post} imgStyle={{ maxHeight: 200 }} />
-        ))}
+  const renderRow = ({ index, style }) => {
+    const post = posts[index]
+
+    return (
+      <li key={post.id} style={style}>
+        <Post
+          key={index}
+          index={index}
+          post={post}
+          imgStyle={{ maxHeight: 200 }}
+        />
+      </li>
+    )
+  }
+
+  return (
+    <ul>
+      <List
+        itemCount={posts.length}
+        itemSize={270}
+        width={window.innerWidth}
+        height={window.innerHeight}
+      >
+        {renderRow}
+      </List>
     </ul>
+  )
 })
